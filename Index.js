dcad9eba4a08fd751b6457e80669028519d0c8ab
EgorDorodnyi <dorodnyie@wsc.school.nz>
2026-07-01 15:03:22 +1200

    added prototype marker
    prototer marker

--- Index.js
+++ Index.js
@@ -1,11 +1,12 @@
-// Centered on New Zealand
-var map = L.map('map').setView([-41.2865, 174.7762], 6);
+// centered on auckland
+var map = L.map('map').setView([-36.85133, 174.76345], 12);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Example fishing spot marker
-L.marker([-36.8485, 174.7633]).addTo(map)
-    .bindPopup('Auckland fishing spot');
+//fishing spot test marker
+L.marker([-36.84072, 174.73182]).addTo(map)
+    .bindPopup('<a href="fishingSpots/ponsonbySpot.html">Info</a>');
+
